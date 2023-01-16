8b8e9510f4439698271972f753267dbb12588614
makaralis <alisa@monkeytech.co.il>
2023-01-16 11:22:31 +0200

    create register handler

---
+++ api/controllers/auth.js
@@ -0,0 +1,23 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res, next) => {
+    try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync("B4c0/\/", salt);
+
+        const newUser = new User(
+            {
+                username: req.body.username,
+                email: req.body.email,
+                password: hash
+            }
+        );
+
+        await newUser.save();
+        res.status(200).send("User has been created");
+    }
+    catch (e) {
+        next (e);
+    }
+}
