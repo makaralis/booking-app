3b5c04301297a45123714ffe28b37188fbe69c88
makaralis <makaralice97@gmail.com>
2023-01-22 15:00:34 +0200

    added verifyToken and verifyUser middlewares

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,17 @@
 import express from 'express';
 
 import { deleteUser, getAllUsers, getUser, updateUser } from '../controllers/user.js';
+import { verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
+
+router.get("/checkauth", verifyToken, (req, res, next) => {
+    res.send("hello user, you are authenticated");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+    res.send("hello user, you are logged in and you can delete your account");
+});
 
 // UPDATE
 router.put("/:id", updateUser);
